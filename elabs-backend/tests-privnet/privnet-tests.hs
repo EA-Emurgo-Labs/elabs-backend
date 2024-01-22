806a97ef15b217f122bc92b10d415ecde487fc35
Piyush thapa <piyush.codelabs@gmail.com>
2024-01-22 14:38:46 +0545

    setup privnet-test  for OneShot Mint

--- elabs-backend/tests-privnet/privnet-tests.hs
+++ elabs-backend/tests-privnet/privnet-tests.hs
@@ -1,9 +1,16 @@
 module Main (main) where
 
+import qualified EA.Test.Privnet.OneShotMint
 import GeniusYield.Test.Privnet.Setup
 import Test.Tasty (defaultMain, testGroup, withResource)
 
 main :: IO ()
 main = do
-  defaultMain $ withResource makeSetup (const mempty) $ \_setup -> do
-    testGroup "privnet Test" []
+  defaultMain $ withResource makeSetup (const mempty) $ \setup -> do
+    testGroup
+      "privnet Tests"
+      [ testGroup
+          "OneShotMint"
+          [ EA.Test.Privnet.OneShotMint.tests setup
+          ]
+      ]
