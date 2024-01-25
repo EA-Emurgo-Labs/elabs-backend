baf86bcb3e115aae497f84ec0db418e27764eba7
edgr <edgr@pm.me>
2024-01-25 17:53:22 +0100

    Integration setup, db migration, internal collateral

--- elabs-backend/integration-test/Setup.hs
+++ elabs-backend/integration-test/Setup.hs
@@ -6,15 +6,29 @@ module Setup (
 import Control.Monad.Logger (runStderrLoggingT)
 import Control.Monad.Metrics qualified as Metrics
 import Data.Text qualified as T
-import Database.Persist.Sqlite (createSqlitePool)
-import EA (EAAppEnv (..))
+import Database.Persist.Sqlite (createSqlitePool, runSqlPool)
+import EA (EAAppEnv (..), eaLiftMaybe, runEAApp)
 import EA.Script (Scripts (Scripts))
 import EA.Test.Helpers (createRootKey)
-import GeniusYield.Test.Privnet.Ctx (Ctx (..), ctxProviders)
+import EA.Wallet (eaGetInternalAddresses)
+import GeniusYield.Test.Privnet.Ctx (
+  Ctx (..),
+  ctxProviders,
+  ctxRunI,
+  submitTx,
+ )
 import GeniusYield.Test.Privnet.Setup (Setup, withSetup)
-import GeniusYield.Types (GYNetworkId (GYPrivnet))
+import GeniusYield.TxBuilder (mustHaveOutput)
+import GeniusYield.Types (
+  GYNetworkId (GYPrivnet),
+  GYTxOut (GYTxOut),
+  valueFromLovelace,
+ )
+import Internal.Wallet.DB.Sqlite
 import Ply (readTypedScript)
 import System.Directory (doesFileExist, removeFile)
+
+--------------------------------------------------------------------------------
 
 data EACtx = EACtx
   { eaCtxCtx :: Ctx
@@ -58,5 +72,24 @@ withEASetup ioSetup putLog kont =
           , eaAppEnvSqlPool = pool
           , eaAppEnvRootKey = rootKey
           }
-    -- TODO: send 5 Ada to collaternal
+
+    -- DB migrations
+    void $ runSqlPool (runAutoMigration >> createAccount) pool
+
+    -- Adding funds to the internal collateral address
+    txId <- runEAApp env $ do
+      addrs <- eaGetInternalAddresses
+      (addr, _) <-
+        eaLiftMaybe "No internal address found" $
+          viaNonEmpty head addrs
+      let
+        user = ctxUser2 ctx
+        tx =
+          mustHaveOutput
+            (GYTxOut addr (valueFromLovelace 5_000_000) Nothing Nothing)
+      txBody <- liftIO $ ctxRunI ctx user $ return tx
+      liftIO $ submitTx ctx user txBody
+
+    putLog $ "Send 5 Ada to the collateral address: " <> show txId
+
     kont $ EACtx ctx env
