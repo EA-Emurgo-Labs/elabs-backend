ccc614d295672e2fc5aeee2a556944b79ae31bd6
edgr <edgr@pm.me>
2024-02-05 12:18:39 +0100

    Formatting

--- elabs-backend/src/Internal/Ipfs.hs
+++ elabs-backend/src/Internal/Ipfs.hs
@@ -1,24 +1,50 @@
 {-# OPTIONS_GHC -Wno-missing-signatures #-}
+
 module Internal.Ipfs (
-  ipfsPinnedObjects
-, ipfsAddFile
-, ipfsPinObject
+  ipfsPinnedObjects,
+  ipfsAddFile,
+  ipfsPinObject,
 ) where
 
-
-import Servant ( JSON, type (:>), Header, Get, Post, (:<|>) ((:<|>)), Capture)
-import Internal.Ipfs.Types (IpfsListResponse, IpfsAddResponse, IpfsPin)
-import Servant.Client (ClientM, client, mkClientEnv, BaseUrl (BaseUrl), Scheme (Https), runClientM, ClientError)
+import Control.Exception (ErrorCall (ErrorCall))
+import Data.ByteString.Lazy qualified as LBS
+import Data.Text qualified as T
+import EA (
+  EAApp,
+  EAAppEnv (eaAppEnvBlockfrostIpfsProjectId),
+  eaLiftEither,
+  eaThrow,
+ )
+import Internal.Ipfs.Types (IpfsAddResponse, IpfsListResponse, IpfsPin)
 import Network.HTTP.Client (newManager)
 import Network.HTTP.Client.TLS (tlsManagerSettings)
-import EA (EAApp, eaLiftEither, EAAppEnv (eaAppEnvBlockfrostIpfsProjectId), eaThrow)
-import Servant.Multipart (MultipartForm, MultipartData (MultipartData), Tmp, FileData (FileData))
+import Servant (
+  Capture,
+  Get,
+  Header,
+  JSON,
+  Post,
+  (:<|>) ((:<|>)),
+  type (:>),
+ )
+import Servant.Client (
+  BaseUrl (BaseUrl),
+  ClientError,
+  ClientM,
+  Scheme (Https),
+  client,
+  mkClientEnv,
+  runClientM,
+ )
+import Servant.Multipart (
+  FileData (FileData),
+  MultipartData (MultipartData),
+  MultipartForm,
+  Tmp,
+ )
 import Servant.Multipart.Client ()
-import qualified Data.Text as T
 import System.Directory (doesFileExist)
 import System.FilePath (takeBaseName)
-import qualified Data.ByteString.Lazy as LBS
-import Control.Exception (ErrorCall(ErrorCall))
 
 --------------------------------------------------------------------------------
 
@@ -46,25 +72,25 @@ type IpfsAdd =
 api :: Proxy BlockfrostIpfsApi
 api = Proxy
 
-
 ipfsList :<|> ipfsPin :<|> ipfsAdd = client api
-
 
 -- | Run the client
 runClient' :: ClientM a -> IO (Either ClientError a)
 runClient' client = do
   manager <- newManager tlsManagerSettings
   let
-    env = mkClientEnv
-      manager (BaseUrl Https "ipfs.blockfrost.io" 443 "api/v0/ipfs")
+    env =
+      mkClientEnv
+        manager
+        (BaseUrl Https "ipfs.blockfrost.io" 443 "api/v0/ipfs")
   runClientM client env
 
 -- | List pinned objects
 ipfsPinnedObjects :: EAApp [IpfsListResponse]
 ipfsPinnedObjects = do
   token <- asks eaAppEnvBlockfrostIpfsProjectId
-  eaLiftEither show =<<
-    (liftIO . runClient' $ ipfsList (Just token))
+  eaLiftEither show
+    =<< (liftIO . runClient' $ ipfsList (Just token))
 
 -- | Add a file to IPFS
 ipfsAddFile :: FilePath -> EAApp IpfsAddResponse
@@ -80,16 +106,15 @@ ipfsAddFile fp = do
           MultipartData
             []
             [FileData "file" fn "application/octet-stream" fp]
-      eaLiftEither show =<<
-        ( liftIO . runClient'
-            $ ipfsAdd (Just token) ("----EalabsBackendBoundary", multipart)
-        )
-    else
-      eaThrow . ErrorCall $ "File not found: " ++ fp
+      eaLiftEither show
+        =<< ( liftIO . runClient' $
+                ipfsAdd (Just token) ("----EalabsBackendBoundary", multipart)
+            )
+    else eaThrow . ErrorCall $ "File not found: " ++ fp
 
 -- | Pin an object
 ipfsPinObject :: Text -> EAApp IpfsPin
 ipfsPinObject path = do
   token <- asks eaAppEnvBlockfrostIpfsProjectId
-  eaLiftEither show =<<
-    (liftIO . runClient' $ ipfsPin (Just token) path)
+  eaLiftEither show
+    =<< (liftIO . runClient' $ ipfsPin (Just token) path)
