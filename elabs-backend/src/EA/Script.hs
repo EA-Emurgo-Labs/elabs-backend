79bac52686836c1c7ed45ae1f404f5d425002f37
edgr <edgr@pm.me>
2024-02-06 11:56:36 +0100

    Script fixes

--- elabs-backend/src/EA/Script.hs
+++ elabs-backend/src/EA/Script.hs
@@ -2,9 +2,16 @@ module EA.Script (Scripts (..), oneShotMintingPolicy) where
 
 import EA.Internal (mintingPolicyFromPly)
 import GeniusYield.Types
+import PlutusLedgerApi.V1 (PubKeyHash, ScriptHash, TokenName, TxOutRef)
 import PlutusLedgerApi.V1.Value (AssetClass)
-import PlutusLedgerApi.V2 (PubKeyHash, ScriptHash, TokenName, TxId, TxOutRef)
-import Ply (AsData (AsData), ScriptRole (MintingPolicyRole, ValidatorRole), TypedScript, (#))
+
+import PlutusLedgerApi.V1.Tx (TxId)
+import Ply (
+  AsData (AsData),
+  ScriptRole (MintingPolicyRole, ValidatorRole),
+  TypedScript,
+  (#),
+ )
 
 data Scripts = Scripts
   { scriptsOneShotPolicy :: !(TypedScript 'MintingPolicyRole '[AsData TxOutRef])
