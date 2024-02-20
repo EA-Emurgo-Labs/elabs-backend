646956d2c10f451c511094c0b991945eda4d1356
Piyush thapa <piyush.codelabs@gmail.com>
2024-02-20 07:39:34 +0545

    marketplace Info type

--- elabs-backend/src/EA/Tx/Changeblock/Marketplace.hs
+++ elabs-backend/src/EA/Tx/Changeblock/Marketplace.hs
@@ -1,1 +1,16 @@
-module EA.Tx.Changeblock.Marketplace () where
+module EA.Tx.Changeblock.Marketplace (MarketplaceInfo (..)) where
+
+import GeniusYield.Types (GYMintingPolicyId, GYPubKeyHash, GYTokenName, GYTxOutRef, GYValue)
+
+data MarketplaceInfo = MarketplaceInfo
+  { mktInfoTxOutRef :: GYTxOutRef
+  , mktInfoValue :: GYValue
+  , mktInfoOwner :: GYPubKeyHash
+  , mktInfoSalePrice :: Integer
+  , mktInfoCarbonPolicyId :: GYMintingPolicyId
+  , mktInfoCarbonAssetName :: GYTokenName
+  , mktInfoAmount :: Integer
+  , mktInfoIssuer :: GYPubKeyHash
+  , mktInfoIsSell :: Integer
+  }
+  deriving stock (Show)
