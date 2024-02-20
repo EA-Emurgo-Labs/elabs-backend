08c9ed76e85a15fac9d634b4eb647cd9b22ddca0
thinhnguyen <thinhnq@blockchainlabs.asia>
2024-02-20 17:58:54 +0700

    marketplaceAtTxOutref

--- elabs-backend/src/EA/Tx/Changeblock/Marketplace.hs
+++ elabs-backend/src/EA/Tx/Changeblock/Marketplace.hs
@@ -4,8 +4,9 @@ import EA.Script (Scripts, marketplaceValidator)
 import EA.Script.Marketplace (MarketplaceDatum (..), MarketplaceInfo (..), MarketplaceParams (..), marketplaceInfoToDatum)
 import EA.Script.Marketplace qualified as MarketplaceAction
 import EA.Script.Oracle (OracleInfo (..))
-import GeniusYield.TxBuilder (GYTxSkeleton, mustBeSignedBy, mustHaveInput, mustHaveOutput, mustHaveRefInput)
+import GeniusYield.TxBuilder (GYTxSkeleton, mustBeSignedBy, mustHaveInput, mustHaveOutput, mustHaveRefInput, utxoDatumPure)
 import GeniusYield.Types
+import EA
 
 type BuyerPubkeyHash = GYPubKeyHash
 
@@ -44,3 +45,28 @@ buy nid info@MarketplaceInfo {..} OracleInfo {..} buyerPubKeyHash mMarketplaceRe
         , mktDtmIssuer = pubKeyHashToPlutus mktInfoIssuer
         , mktDtmIsSell = 0
         }
+
+marketplaceAtTxOutRef :: GYTxOutRef -> EAApp( Either String MarketplaceInfo)
+marketplaceAtTxOutRef oref = do
+    providers <- asks eaAppEnvGYProviders
+    [utxo] <- liftIO $ gyQueryUtxosAtTxOutRefsWithDatums providers [oref]
+    let result = utxoDatumPure utxo
+
+    return $ case result of
+        Left msg -> Left "Datum not found"
+        Right datum -> 
+                let (addr, val, MarketplaceDatum owner salePrice  assetCS  assetTN   assetAmount issuer   isSell) = datum
+                    Right pubkeyIssue = pubKeyHashFromPlutus issuer  
+                    Right pubkeyOwner = pubKeyHashFromPlutus owner  
+                    info = MarketplaceInfo {
+                        mktInfoTxOutRef = oref
+                        , mktInfoValue = val
+                        , mktInfoOwner = pubkeyOwner
+                        , mktInfoSalePrice = salePrice
+                        , mktInfoCarbonPolicyId = assetCS
+                        , mktInfoCarbonAssetName = assetTN
+                        , mktInfoAmount = assetAmount
+                        , mktInfoIssuer = pubkeyIssue 
+                        , mktInfoIsSell = isSell
+                        }
+                in Right info
