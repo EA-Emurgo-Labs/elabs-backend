57b6f0a5d52f579ca0561044e5e5cb2e45b306ff
thinhnguyen <thinhnq@blockchainlabs.asia>
2024-02-20 10:40:20 +0700

    temp

--- elabs-backend/src/EA/Tx/Changeblock/Marketplace.hs
+++ elabs-backend/src/EA/Tx/Changeblock/Marketplace.hs
@@ -1,6 +1,7 @@
 module EA.Tx.Changeblock.Marketplace (MarketplaceInfo (..)) where
 
-import GeniusYield.Types (GYMintingPolicyId, GYPubKeyHash, GYTokenName, GYTxOutRef, GYValue)
+import EA (EAApp, EAAppEnv (..), eaAppEnvSqlPool, eaGetCollateral, eaLiftEither)
+import GeniusYield.Types (GYMintingPolicyId, GYPubKeyHash, GYTokenName, GYTxOutRef, GYValue, gyQueryUtxosAtTxOutRefsWithDatums)
 
 data MarketplaceInfo = MarketplaceInfo
   { mktInfoTxOutRef :: GYTxOutRef
@@ -16,4 +17,27 @@ data MarketplaceInfo = MarketplaceInfo
   deriving stock (Show)
 
 marketplaceAtTxOutref :: GYTxOutRef ->  Either String MarketplaceInfo
+marketplaceAtTxOutref oref = do
+    providers <- asks eaAppEnvGYProviders
+    [(utxo, datum)] <- liftIO $ gyQueryUtxosAtTxOutRefsWithDatums providers [oref]
+    case datum of
+        Nothing -> Left "Datum not found"
+        Just ( owner         
+    , salePrice  
+    , assetCS    
+    , assetTN    
+    , assetAmount 
+    , issuer     
+    , isSell ) -> Right (MarketplaceInfo {
+        mktInfoTxOutRef = oref
+  , mktInfoValue = utxoValue utxo
+  , mktInfoOwner = owner
+  , mktInfoSalePrice = salePrice
+  , mktInfoCarbonPolicyId = assetCS
+  , mktInfoCarbonAssetName = assetTN
+  , mktInfoAmount = assetAmount
+  , mktInfoIssuer = issuer   
+  , mktInfoIsSell = isSell
+    })
+
 
