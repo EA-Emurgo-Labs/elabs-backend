5d55487ea6e2bcf64b384490699e724ff04b41d9
Piyush thapa <piyush.codelabs@gmail.com>
2024-02-20 15:38:17 +0545

    marketplace Buy action

--- elabs-backend/src/EA/Tx/Changeblock/Marketplace.hs
+++ elabs-backend/src/EA/Tx/Changeblock/Marketplace.hs
@@ -1,16 +1,46 @@
-module EA.Tx.Changeblock.Marketplace (MarketplaceInfo (..)) where
+module EA.Tx.Changeblock.Marketplace (MarketplaceInfo (..), buy) where
 
-import GeniusYield.Types (GYMintingPolicyId, GYPubKeyHash, GYTokenName, GYTxOutRef, GYValue)
+import EA.Script (Scripts, marketplaceValidator)
+import EA.Script.Marketplace (MarketplaceDatum (..), MarketplaceInfo (..), MarketplaceParams (..), marketplaceInfoToDatum)
+import EA.Script.Marketplace qualified as MarketplaceAction
+import EA.Script.Oracle (OracleInfo (..))
+import GeniusYield.TxBuilder (GYTxSkeleton, mustBeSignedBy, mustHaveInput, mustHaveOutput, mustHaveRefInput)
+import GeniusYield.Types
 
-data MarketplaceInfo = MarketplaceInfo
-  { mktInfoTxOutRef :: GYTxOutRef
-  , mktInfoValue :: GYValue
-  , mktInfoOwner :: GYPubKeyHash
-  , mktInfoSalePrice :: Integer
-  , mktInfoCarbonPolicyId :: GYMintingPolicyId
-  , mktInfoCarbonAssetName :: GYTokenName
-  , mktInfoAmount :: Integer
-  , mktInfoIssuer :: GYPubKeyHash
-  , mktInfoIsSell :: Integer
-  }
-  deriving stock (Show)
+type BuyerPubkeyHash = GYPubKeyHash
+
+buy :: GYNetworkId -> MarketplaceInfo -> OracleInfo -> BuyerPubkeyHash -> Maybe GYTxOutRef -> MarketplaceParams -> Scripts -> GYTxSkeleton 'PlutusV2
+buy nid info@MarketplaceInfo {..} OracleInfo {..} buyerPubKeyHash mMarketplaceRefScript mktPlaceParams scripts =
+  let mktPlaceValidator = marketplaceValidator mktPlaceParams scripts
+      marketplaceAddr = addressFromValidator nid mktPlaceValidator
+      marketOutValue = valueSingleton (GYToken mktInfoCarbonPolicyId mktInfoCarbonAssetName) mktInfoAmount
+      escrowAddress = addressFromPubKeyHash nid (mktPrmEscrowValidator mktPlaceParams)
+      ownerAddress = addressFromPubKeyHash nid mktInfoOwner
+   in mustHaveRefInput orcInfoUtxoRef
+        <> mustHaveInput (input mktPlaceValidator)
+        <> mustHaveOutput (mkGYTxOutNoDatum ownerAddress (valueFromLovelace (mktInfoSalePrice * mktInfoAmount)))
+        <> mustHaveOutput (mkGYTxOut marketplaceAddr marketOutValue (datumFromPlutusData newDatum))
+        <> mustHaveOutput (mkGYTxOutNoDatum escrowAddress (valueFromLovelace orcInfoRate))
+        <> mustBeSignedBy buyerPubKeyHash
+  where
+    input validator =
+      GYTxIn
+        { gyTxInTxOutRef = mktInfoTxOutRef
+        , gyTxInWitness =
+            GYTxInWitnessScript
+              (inputWitness validator)
+              (datumFromPlutusData $ marketplaceInfoToDatum info)
+              (redeemerFromPlutusData MarketplaceAction.BUY)
+        }
+    inputWitness validator = maybe (GYInScript validator) (\ref -> GYInReference ref (validatorToScript validator)) mMarketplaceRefScript
+
+    newDatum =
+      MarketplaceDatum
+        { mktDtmOwner = pubKeyHashToPlutus buyerPubKeyHash
+        , mktDtmSalePrice = mktInfoSalePrice
+        , mktDtmAssetSymbol = mintingPolicyIdCurrencySymbol mktInfoCarbonPolicyId
+        , mktDtmAssetName = tokenNameToPlutus mktInfoCarbonAssetName
+        , mktDtmAmount = mktInfoAmount
+        , mktDtmIssuer = pubKeyHashToPlutus mktInfoIssuer
+        , mktDtmIsSell = 0
+        }
