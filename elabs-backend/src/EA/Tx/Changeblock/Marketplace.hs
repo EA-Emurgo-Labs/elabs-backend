b444bfbb3093d9ad8ebd5aa98846126ab560ee65
thinhnguyen <thinhnq@blockchainlabs.asia>
2024-02-20 09:36:23 +0700

    template

--- elabs-backend/src/EA/Tx/Changeblock/Marketplace.hs
+++ elabs-backend/src/EA/Tx/Changeblock/Marketplace.hs
@@ -1,1 +1,23 @@
 module EA.Tx.Changeblock.Marketplace () where
+
+import GeniusYield.Types (
+    GYTxoutRef,
+    GYValue,
+    GYPubKeyHash,
+    GYMintingPolicyId,
+    GYTokenName,
+    )
+
+data MarketplaceInfo = MarketplaceInfo {
+   mktInfOref :: GYTxoutRef,
+   mktInfValue :: GYValue,
+   mktInfOwner  :: GYPubKeyHash,
+   mktInfSalePrice  :: Integer,
+   mktInfCarbonPolicyId :: GYMintingPolicyId,
+   mktInfCarbonTokenName :: GYTokenName,
+   mktInfCarbonTokenAmount :: Integer,
+   mktInfIssuer :: GYPubKeyHash,
+   mktInfIsSell :: Integer
+ }
+
+marketplaceAtTxOutref :: GYTxoutRef ->  Either String MarketplaceInfo
