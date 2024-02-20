554c27d95ac6f070265e9e2afc746d865cfdb523
thinhnguyen <thinhnq@blockchainlabs.asia>
2024-02-20 12:56:52 +0700

    error either

--- elabs-backend/src/EA/Tx/Changeblock/Marketplace.hs
+++ elabs-backend/src/EA/Tx/Changeblock/Marketplace.hs
@@ -1,7 +1,8 @@
 module EA.Tx.Changeblock.Marketplace (MarketplaceInfo (..)) where
 
 import EA (EAApp, EAAppEnv (..), eaAppEnvSqlPool, eaGetCollateral, eaLiftEither)
-import GeniusYield.Types (GYMintingPolicyId, GYPubKeyHash, GYTokenName, GYTxOutRef, GYValue, gyQueryUtxosAtTxOutRefsWithDatums)
+import GeniusYield.Types 
+import GeniusYield.TxBuilder
 
 data MarketplaceInfo = MarketplaceInfo
   { mktInfoTxOutRef :: GYTxOutRef
@@ -16,28 +17,37 @@ data MarketplaceInfo = MarketplaceInfo
   }
   deriving stock (Show)
 
+data MarketplaceDatum = MarketplaceDatum
+  { mktInfoOwner :: GYPubKeyHash
+  , mktInfoSalePrice :: Integer
+  , mktInfoCarbonPolicyId :: GYMintingPolicyId
+  , mktInfoCarbonAssetName :: GYTokenName
+  , mktInfoAmount :: Integer
+  , mktInfoIssuer :: GYPubKeyHash
+  , mktInfoIsSell :: Integer
+  }
+  deriving stock (Show)
+
 marketplaceAtTxOutref :: GYTxOutRef ->  Either String MarketplaceInfo
 marketplaceAtTxOutref oref = do
     providers <- asks eaAppEnvGYProviders
-    [(utxo, datum)] <- liftIO $ gyQueryUtxosAtTxOutRefsWithDatums providers [oref]
-    case datum of
-        Nothing -> Left "Datum not found"
-        Just ( owner         
-    , salePrice  
-    , assetCS    
-    , assetTN    
-    , assetAmount 
-    , issuer     
-    , isSell ) -> Right (MarketplaceInfo {
-        mktInfoTxOutRef = oref
-  , mktInfoValue = utxoValue utxo
-  , mktInfoOwner = owner
-  , mktInfoSalePrice = salePrice
-  , mktInfoCarbonPolicyId = assetCS
-  , mktInfoCarbonAssetName = assetTN
-  , mktInfoAmount = assetAmount
-  , mktInfoIssuer = issuer   
-  , mktInfoIsSell = isSell
-    })
+    [utxo] <- liftIO $ gyQueryUtxosAtTxOutRefsWithDatums providers [oref]
+    result <- utxoDatumPure utxo
 
+    case result of
+        Left msg -> Left msg
+        Right datum -> do
+                let (addr, val, (MarketplaceDatum owner salePrice  assetCS  assetTN   assetAmount issuer   isSell)) = datum
+                let info = MarketplaceInfo {
+                        mktInfoTxOutRef = oref
+                        , mktInfoValue = val
+                        , mktInfoOwner = owner
+                        , mktInfoSalePrice = salePrice
+                        , mktInfoCarbonPolicyId = assetCS
+                        , mktInfoCarbonAssetName = assetTN
+                        , mktInfoAmount = assetAmount
+                        , mktInfoIssuer = issuer   
+                        , mktInfoIsSell = isSell
+                        }
+                return (Right info)
 
