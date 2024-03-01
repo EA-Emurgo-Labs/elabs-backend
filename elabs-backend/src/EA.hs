29472060d3f449846996f7338552dce574806d5d
Piyush thapa <piyush.codelabs@gmail.com>
2024-03-01 20:08:22 +0545

    more env variable

--- elabs-backend/src/EA.hs
+++ elabs-backend/src/EA.hs
@@ -34,28 +34,9 @@ import EA.Script.Marketplace (
   MarketplaceParams,
   marketplaceDatumToInfo,
  )
+import EA.Script.Oracle (OracleInfo)
 import GeniusYield.TxBuilder (adaOnlyUTxOPure, utxoDatumPure)
-import GeniusYield.Types (
-  GYAddress,
-  GYDatum,
-  GYLogNamespace,
-  GYLogSeverity (..),
-  GYNetworkId,
-  GYProviders (..),
-  GYQueryUTxO (..),
-  GYTx,
-  GYTxId,
-  GYTxOutRef,
-  GYUTxO (utxoRef),
-  addressFromValidator,
-  gyLog,
-  gyLogDebug,
-  gyLogError,
-  gyLogInfo,
-  gyLogWarning,
-  gyQueryUtxosAtAddressesWithDatums,
-  gyQueryUtxosAtTxOutRefsWithDatums,
- )
+import GeniusYield.Types
 import Internal.Wallet (RootKey)
 import UnliftIO (MonadUnliftIO (withRunInIO))
 
@@ -86,6 +67,13 @@ data EAAppEnv = EAAppEnv
   , eaAppEnvRootKey :: !RootKey
   , eaAppEnvBlockfrostIpfsProjectId :: !String
   , eaAppEnvAuthTokens :: ![Text]
+  , eaAppEnvOracleRefInputUtxo :: !(Maybe OracleInfo)
+  , eaAppEnvMarketplaceRefScriptUtxo :: !(Maybe GYTxOutRef)
+  , eaAppEnvMarketplaceEscrowPubKeyHash :: !GYPubKeyHash
+  , eaAppEnvMarketplaceVersion :: !GYTokenName
+  , eaAppEnvOracleOperatorPubKeyHash :: !GYPubKeyHash
+  , eaAppEnvOracleNftMintingPolicyId :: !(Maybe GYMintingPolicyId)
+  , eaAppEnvOracleNftTokenName :: !(Maybe GYTokenName)
   }
 
 runEAApp :: EAAppEnv -> EAApp a -> IO a
@@ -205,6 +193,7 @@ eaMarketplaceInfos mktPlaceParams = do
   let mktPlaceValidator = marketplaceValidator mktPlaceParams scripts
       marketplaceAddr = addressFromValidator nid mktPlaceValidator
 
+  putStrLn $ "\n Marketplace address: " ++ show marketplaceAddr
   utxos <-
     liftIO $
       gyQueryUtxosAtAddressesWithDatums providers [marketplaceAddr]
