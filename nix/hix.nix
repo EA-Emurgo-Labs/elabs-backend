728622e130bfb30fc166b0a85d55d0adf6ed90db
piyush <piyush.codelabs@gmail.com>
2024-03-11 13:12:00 +0545

    setup postgresql

--- nix/hix.nix
+++ nix/hix.nix
@@ -1,4 +1,25 @@
-{ pkgs, ... }: {
+{ pkgs, ... }: 
+  let 
+    startDb = pkgs.writeScriptBin "startDb" ''
+      initdb -D $PGDATA
+
+      pg_ctl                                                  \
+        -D $PGDATA                                            \
+        -l $PGDATA/postgres.log                               \
+        -o "-c unix_socket_directories='$PGDATA'"             \
+        -o "-c listen_addresses='*'"                          \
+        -o "-c log_destination='stderr'"                      \
+        -o "-c logging_collector=on"                          \
+        -o "-c log_directory='log'"                           \
+        -o "-c log_filename='postgresql-%Y-%m-%d_%H%M%S.log'" \
+        -o "-c log_min_messages=info"                         \
+        -o "-c log_min_error_statement=info"                  \
+        -o "-c log_connections=on"                            \
+        start
+
+      
+    '';
+in {
   # name = "project-name";
   compiler-nix-name = "ghc928"; # Version of GHC to use
 
@@ -24,6 +45,24 @@
       haskellPackages.fourmolu
       nixpkgs-fmt
       gnumake
+      postgresql_14
+      startDb
     ];
+
+
+
+    # Start and terminate postgresql gracefully
+    shellHook = ''
+      mkdir -p $PWD/.db
+      export PGDATA=$PWD/.db
+      trap \
+        "
+          # Stop PostgreSQL
+          pg_ctl -D $PGDATA stop
+        " \
+      EXIT
+
+
+    '';
   };
 }
