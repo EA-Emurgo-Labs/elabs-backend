500cedbaaf66df626bc21786c85a0770741b5912
Piyush <piyush.codelabs@gmail.com>
2024-05-08 10:26:12 +0545

    add exception to carbon token mint API

--- elabs-backend/src/EA/CommonException.hs
+++ elabs-backend/src/EA/CommonException.hs
@@ -1,23 +1,41 @@
 module EA.CommonException (CommonException (..)) where
 
+import Data.Text qualified as T
+import EA.Api.Types (UserId (..))
 import GeniusYield.HTTP.Errors (GYApiError (..), IsGYApiError (toApiError))
-import GeniusYield.Types (GYAddress, GYPubKeyHash)
+import GeniusYield.Types (GYAddress, GYPubKeyHash, GYTxOutRef)
 import Network.HTTP.Types (status400)
 
 data CommonException
-  = EaNoUtxo {cmnExpEaAddressUtxo :: GYAddress}
+  = EaNoUtxoForAddress {cmnExpEaAddressUtxo :: GYAddress}
+  | EaNoInternalUtxo
+  | EaInvalidTxRef {cmnExpEaTxRef :: GYTxOutRef}
   | EaNoCollateral
   | EaInvalidAddres {cmnExpEaAddress :: GYAddress}
   | EaCannotDecodeAddress {cmnExpEaPubKey :: GYPubKeyHash}
+  | EaInvalidUserAddress {cmnExpEaUserId :: UserId}
+  | EaCustomError {eaCustomErrorCode :: T.Text, eaCustomErrorMsg :: T.Text}
   deriving stock (Show)
   deriving anyclass (Exception)
 
 instance IsGYApiError CommonException where
-  toApiError (EaNoUtxo address) =
+  toApiError (EaCustomError code msg) =
+    GYApiError
+      { gaeHttpStatus = status400
+      , gaeErrorCode = code
+      , gaeMsg = msg
+      }
+  toApiError EaNoInternalUtxo =
+    GYApiError
+      { gaeHttpStatus = status400
+      , gaeErrorCode = "NO_UTXO_FOUND_INTERNAL_ADDRESS"
+      , gaeMsg = "No UTxO found For Internal Address. Send some ADA to Internal Address"
+      }
+  toApiError (EaNoUtxoForAddress address) =
     GYApiError
       { gaeHttpStatus = status400
       , gaeErrorCode = "NO_UTXO_FOUND"
-      , gaeMsg = "No UTxO found for address: " <> show address
+      , gaeMsg = "No UTxO Found for Address: " <> show address
       }
   toApiError EaNoCollateral =
     GYApiError
@@ -36,4 +54,16 @@ instance IsGYApiError CommonException where
       { gaeHttpStatus = status400
       , gaeErrorCode = "CANNOT_DECODE_ADDRESS"
       , gaeMsg = "Cannot decode address for pub key: " <> show pubKey
+      }
+  toApiError (EaInvalidTxRef txRef) =
+    GYApiError
+      { gaeHttpStatus = status400
+      , gaeErrorCode = "INVALID_UTXO"
+      , gaeMsg = "No UTxO Found for TxRef: " <> show txRef
+      }
+  toApiError (EaInvalidUserAddress (UserId uid)) =
+    GYApiError
+      { gaeHttpStatus = status400
+      , gaeErrorCode = "INVALID_USER_ADDRESS"
+      , gaeMsg = "No Address Found for User: " <> show uid
       }
